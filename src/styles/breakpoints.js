d8a1dff81fa7ba74bcaec210df1bff1ee7ea32e2
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-07-05 21:55:44 -0300

    feat: mediaQuery added

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -11,3 +11,16 @@ export const screens = {
   laptop: '1024px',
   desktop: '1280px'
 }
+
+export const mediaQuery = {
+  minXs: `(min-width: ${breakpoints.xs})`,
+  maxXs: `(max-width: ${breakpoints.xs})`,
+  minSm: `(max-width: ${breakpoints.sm})`,
+  maxSm: `(max-width: ${breakpoints.sm})`,
+  minMd: `(max-width: ${breakpoints.md})`,
+  maxMd: `(max-width: ${breakpoints.md})`,
+  minLg: `(max-width: ${breakpoints.lg})`,
+  maxLg: `(max-width: ${breakpoints.lg})`,
+  minXl: `(max-width: ${breakpoints.xl})`,
+  maxXl: `(max-width: ${breakpoints.xl})`
+}
